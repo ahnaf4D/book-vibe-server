198821b4e06ea28f34f2edee43b38f6a6b6a44e9
ahnaf4d <ahnaftahmid802@gmail.com>
2024-06-21 17:29:51 +0600

    blogs data added

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 const books = require('./books.json');
-
+const blogs = require('./blogs.json');
 app.get('/', (req, res) => {
   res.send(books);
+});
+app.get('/blogs', (req, res) => {
+  res.send(blogs);
+});
+
+app.get('/blog/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const blog = blogs.find((item) => item.id === id);
+  res.send(blog);
 });
 app.get('/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const book = books.find((item) => item.bookId === id);
   res.send(book);
 });
+
 app.listen(port, () => {
   console.log(`server listening on ${port}...`);
 });
